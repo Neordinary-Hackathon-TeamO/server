2fb3520bd9afddfd44803c4d3a7b3c7edef8f3a8
Kang Chaewon <kang960953@gmail.com>
2024-11-23 20:35:30 +0900

    Feat, Fix: add team service, exists member join error

--- src/main/java/org/example/secret_santa/team/dto/TeamRequestDto.java
+++ src/main/java/org/example/secret_santa/team/dto/TeamRequestDto.java
@@ -2,6 +2,7 @@ package org.example.secret_santa.team.dto;
 
 import lombok.Data;
 import org.example.secret_santa.common.enums.TeamType;
+import org.example.secret_santa.team.entity.Team;
 
 import java.time.LocalDateTime;
 
@@ -11,13 +12,23 @@ public class TeamRequestDto {
     public static class AddTeamDto {
         String name;
 
-        TeamType type;
+        String type;
+        Integer headCount;
         LocalDateTime startDate;
         LocalDateTime endDate;
 
+        public static Team convertDtoToTeam(AddTeamDto dto ) {
+            return Team.builder()
+                    .teamType(TeamType.toType(dto.type))
+                    .headCount(dto.headCount)
+                    .startDate(dto.startDate)
+                    .endDate(dto.endDate)
+                    .name(dto.name)
+                    .build();
+        }
+
 
     }
-
 
 
 }
--- src/main/java/org/example/secret_santa/team/entity/Team.java
+++ src/main/java/org/example/secret_santa/team/entity/Team.java
@@ -1,9 +1,7 @@
 package org.example.secret_santa.team.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.example.secret_santa.common.BaseEntity;
 import org.example.secret_santa.common.enums.TeamType;
 import org.example.secret_santa.team.mapping.MemberTeam;
@@ -15,20 +13,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Builder
 @Setter
 @Getter
 @NoArgsConstructor
-@DynamicInsert // 자동으로 insert문에 null값을 배제하고 쿼리문을 날려줌.
-@Table(name = "team")
+@AllArgsConstructor
+//@DynamicInsert // 자동으로 insert문에 null값을 배제하고 쿼리문을 날려줌.
+@Table(name = "teams")
 public class Team extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long teamId;
 
-    @ColumnDefault("비밀 산타")
     String name;
     @Enumerated(EnumType.STRING)
     TeamType teamType;
+    @Column(nullable = false)
+    Integer headCount;
     @Column(nullable = false)
     LocalDateTime startDate;
     @Column(nullable = false)
